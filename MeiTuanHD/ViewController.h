8ffe6377a7ff0db2e1d7d535042892d29834e31a
hansonboy <jianwei2013@126.com>
2016-03-28 21:16:37 +0800

    Initial Commit

---
+++ MeiTuanHD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeiTuanHD
+//
+//  Created by wangjianwei on 16/3/28.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MeiTuanHDTests/MeiTuanHDTests.m
@@ -0,0 +1,39 @@
+//
+//  MeiTuanHDTests.m
+//  MeiTuanHDTests
+//
+//  Created by wangjianwei on 16/3/28.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MeiTuanHDTests : XCTestCase
+
+@end
+
+@implementation MeiTuanHDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
